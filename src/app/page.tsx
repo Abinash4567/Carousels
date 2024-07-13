334eb11926d6dc286a4f7ae98680db6aa4081e9a
Abinash4567 <abinashyadav4567@gmail.com>
2024-07-14 01:55:26 +0530

    added childDiv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,75 @@
 'use client';
 
 import React, { use, useEffect, useRef, useState } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion, useScroll, useTransform, useMotionValueEvent } from 'framer-motion';
 import Image from 'next/image';
 import image from "../../public/tweet_image.jpg"
 
 const Home = () => {
   const parentDiv = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: parentDiv,
-    // offset: ["start start", "end end"]
+  const childDiv = useRef(null);
+
+  const [progress, setProgress] = useState(0);
+  const [progress1, setProgress1] = useState(0);
+
+
+
+  const { scrollYProgress: childYProgress } = useScroll({
+    target: childDiv,
+    offset: ["start start", "end start"]
   });
 
-  const opacity = useTransform( scrollYProgress, [1, 0.8, 0.5, 0], [0, 0.4, 1, 1] )
+  const { scrollYProgress } = useScroll({
+    target: parentDiv,
+    offset: ["start end", "start start"],
+  });
 
+  useMotionValueEvent(scrollYProgress, "change", (value) => {
+    setProgress(value);
+  });
+
+  useMotionValueEvent(childYProgress, "change", (value) => {
+    setProgress1(value);
+  });
+
+  const opacity = useTransform( scrollYProgress, [0, 0.7, 1], [0, 0, 1]);
+  const x = useTransform(childYProgress, [0, 1], ["0%", "90%"])
+;
   return (
-    <div className='h-[500vh]'>
-      <div className='bg-slate-800 h-[100vh]'></div>
+    <div className='h-[1000vh]'>
+      <div className='bg-slate-900 h-[200vh] border-2 border-red-400'></div>
 
-    <motion.div ref={parentDiv} style={{opacity}} className='flex border-2'>
-      <div className='brder-2 items-center flex px-24 basis-1/2'>
-        <div>
-        <h1 className="text-[#f5f5fd] text-4xl">billops</h1>
-        <div className="text-sm text-green-500 font-bold mb-4">
-          Next.js, Typescript, Prisma, Shadcn, NextAuth, Postgresql
+        <div ref={parentDiv} className='h-[500vh] border-2 border-yellow-500'>
+          <motion.div ref={childDiv} style={{opacity}} className='flex border-2 top-0 sticky'>
+
+            <div className='border-2 px-24 w-[50vw] top-44'>
+              <div>
+                <h1 className="text-[#f5f5fd] text-4xl mt-48">billops</h1>
+                <div className="text-sm text-green-500 font-bold mb-4">
+                  Next.js, Typescript, Prisma, Shadcn, NextAuth, Postgresql
+                </div>
+                <div className='text-gray-500 font-semibold'>
+                  billops is a subscription management platform for businesses. It helps businesses manage their subscription models, billing, and user data. Billops offers a dashboard with analytics like revenue, subscribers, and retention rate. Businesses can also manage their subscriptions, coupons, and user details. Billops provides a secure API for organizations to access their data and webhooks to capture payments.
+                </div>
+              </div>
+            </div>
+
+
+            <div className='border-2 w-[50vw] mt-32 overflow-hidden'>
+            <motion.div style={{x}}>
+              <Image 
+              src={image}
+              width={520}
+              alt='Image' />
+            </motion.div>
+            </div>
+          </motion.div>
         </div>
-        <div className='text-gray-500 font-semibold'>
-          billops is a subscription management platform for businesses. It helps businesses manage their subscription models, billing, and user data. Billops offers a dashboard with analytics like revenue, subscribers, and retention rate. Businesses can also manage their subscriptions, coupons, and user details. Billops provides a secure API for organizations to access their data and webhooks to capture payments.
+
+
+        <div className="text-xl fixed top-2 bg-slate-900/70 text-white rounded px-2 py-1">scrollYProgress: {progress.toFixed(4)}
+        <span className='ml-12'>{progress1.toFixed(4)}</span>
         </div>
-        </div>
-
-      </div>
-
-
-      <div className='flex items-center justify-center bordr-2 basis-1/2'>
-        <Image 
-        src={image}
-        width={450}
-        height={1200}
-        alt='Image' />
-      </div>
-
-
-    </motion.div>
   </div>
   );
 };
